e452f9564475d65f3b9ba1b50088a3e51eaa5c2e
Suryaprakash3232 <suryaprakash63v@gmail.com>
2024-09-10 22:01:57 +0530

    Add files via upload

---
+++ odd_nums.java
@@ -0,0 +1,12 @@
+public class odd_nums {
+    public static void main(String[] args)
+    {
+        for(int i=0;i<=10;i=i+1)
+        {
+            if(i%2!=0)
+                System.out.print(i+" ");
+        }
+    }
+
+}
+
